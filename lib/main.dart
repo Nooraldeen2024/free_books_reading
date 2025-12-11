a2d4defe70c5bf7c2abcfabb5928376a03085513
Nooraldeen Al_Awsaji <ngalawsaji@gmail.com>
2025-12-11 19:29:18 +0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:free_books_reading/Features/presentation/view/splash_view.dart';
+import 'package:get/get_navigation/get_navigation.dart';
+
+void main() {
+  runApp(const BooklyApp());
+}
+
+class BooklyApp extends StatelessWidget {
+  const BooklyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(home: SplashView()); 
+  }
+}
