ae98dccae2d820d1093a7e0f033ace836d62f805
Nooraldeen Al_Awsaji <ngalawsaji@gmail.com>
2025-12-13 19:40:45 +0300

    best seller list view item part view

--- lib/Features/presentation/view/widgets/home_view_body.dart
+++ lib/Features/presentation/view/widgets/home_view_body.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:free_books_reading/Features/presentation/view/widgets/custom_app_bar.dart';
 import 'package:free_books_reading/Features/presentation/view/widgets/featured_list_view.dart';
+import 'package:free_books_reading/core/utils/assets.dart';
 import 'package:free_books_reading/core/utils/style.dart';
 
 class HomeViewBody extends StatelessWidget {
@@ -17,6 +18,38 @@ class HomeViewBody extends StatelessWidget {
           FeatureedBooksListView(),
           SizedBox(height: 40),
           Text('Best Seller', style: Styles.titleMedium),
+          BeatSellerListViewItem(),
+        ],
+      ),
+    );
+  }
+}
+
+class BeatSellerListViewItem extends StatelessWidget {
+  const BeatSellerListViewItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 125,
+      child: Row(
+        children: [
+          AspectRatio(
+            aspectRatio: 2.8 / 4,
+            child: Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(16),
+                color: Colors.red,
+                image: const DecorationImage(
+                  fit: BoxFit.fill,
+                  image: AssetImage(AssetsData.testimage),
+                ),
+              ),
+            ),
+          ),
+          Column(children: [
+        
+      ],),
         ],
       ),
     );
