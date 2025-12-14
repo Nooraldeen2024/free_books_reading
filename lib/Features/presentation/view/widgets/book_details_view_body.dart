72bb4592350110b1da98291e7c3fb98e75725483
Nooraldeen Al_Awsaji <ngalawsaji@gmail.com>
2025-12-14 16:53:29 +0300

    create custom book details app bar

--- lib/Features/presentation/view/widgets/book_details_view_body.dart
+++ lib/Features/presentation/view/widgets/book_details_view_body.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:free_books_reading/Features/presentation/view/widgets/Custom_book_details_app_bar.dart';
 
 class BookDetailsViewBody extends StatelessWidget {
   const BookDetailsViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 30),
+      child: Column(children: [CustomBookDetailsAppBar()]),
+    );
   }
 }
--- lib/core/utils/app_router.dart
+++ lib/core/utils/app_router.dart
@@ -1,13 +1,19 @@
 import 'package:free_books_reading/Features/home/presentation/views/home_view.dart';
 import 'package:free_books_reading/Features/presentation/view/splash_view.dart';
+import 'package:free_books_reading/Features/presentation/view/widgets/book_details_view.dart';
 import 'package:go_router/go_router.dart';
 
 abstract class AppRouter {
   static const kHomeView = '/homeView';
+  static const kBookDetailsView = '/BookDetailsView';
   static final router = GoRouter(
     routes: [
       GoRoute(path: '/', builder: (context, state) => const SplashView()),
       GoRoute(path: kHomeView, builder: (context, state) => const HomeView()),
+      GoRoute(
+        path: kBookDetailsView,
+        builder: (context, state) => const BookDetailsView(),
+      ),
     ],
   );
 }
