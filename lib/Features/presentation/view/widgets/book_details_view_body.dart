a07b4954e0a0e89f9d55fb14c72a8d33bef4dd80
Nooraldeen Al_Awsaji <ngalawsaji@gmail.com>
2025-12-14 17:20:31 +0300

    add custom  book item to book details view

--- lib/Features/presentation/view/widgets/book_details_view_body.dart
+++ lib/Features/presentation/view/widgets/book_details_view_body.dart
@@ -1,14 +1,24 @@
 import 'package:flutter/material.dart';
-import 'package:free_books_reading/Features/presentation/view/widgets/Custom_book_details_app_bar.dart';
+import 'package:free_books_reading/Features/presentation/view/widgets/custom_book_details_app_bar.dart';
+import 'package:free_books_reading/Features/presentation/view/widgets/custom_book_item.dart';
 
 class BookDetailsViewBody extends StatelessWidget {
   const BookDetailsViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
+    var width = MediaQuery.of(context).size.width;
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 30),
-      child: Column(children: [CustomBookDetailsAppBar()]),
+      child: Column(
+        children: [
+          CustomBookDetailsAppBar(),
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: width * .15),
+            child: CustomBookImage(),
+          ),
+        ],
+      ),
     );
   }
 }
--- lib/Features/presentation/view/widgets/featured_list_view.dart
+++ lib/Features/presentation/view/widgets/featured_list_view.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:free_books_reading/Features/presentation/view/widgets/custom_list_view.dart';
+import 'package:free_books_reading/Features/presentation/view/widgets/custom_book_item.dart';
 
 class FeatureedBooksListView extends StatelessWidget {
   const FeatureedBooksListView({super.key});
@@ -11,7 +11,7 @@ class FeatureedBooksListView extends StatelessWidget {
       child: ListView.builder(
         scrollDirection: Axis.horizontal,
         itemBuilder: (context, index) {
-          return FeaturedListViewItem();
+          return CustomBookImage();
         },
       ),
     );
