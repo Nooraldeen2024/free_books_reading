92e90640848ac22fb80d5c8e58b117a3d3d4ca34
Nooraldeen Al_Awsaji <ngalawsaji@gmail.com>
2025-12-14 18:05:49 +0300

    add rating to book details

--- lib/Features/presentation/view/widgets/book_details_view_body.dart
+++ lib/Features/presentation/view/widgets/book_details_view_body.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:free_books_reading/Features/presentation/view/widgets/book_rating.dart';
 import 'package:free_books_reading/Features/presentation/view/widgets/custom_book_details_app_bar.dart';
 import 'package:free_books_reading/Features/presentation/view/widgets/custom_book_item.dart';
 import 'package:free_books_reading/core/utils/style.dart';
@@ -25,6 +26,8 @@ class BookDetailsViewBody extends StatelessWidget {
             opacity: 0.7,
             child: Text('Rudyard kipling', style: Styles.txtStyle18),
           ),
+          const SizedBox(height: 18),
+          BookRating(mainAxisAlignment: MainAxisAlignment.center),
         ],
       ),
     );
--- lib/Features/presentation/view/widgets/book_rating.dart
+++ lib/Features/presentation/view/widgets/book_rating.dart
@@ -2,20 +2,21 @@ import 'package:flutter/material.dart';
 import 'package:free_books_reading/core/utils/style.dart';
 
 class BookRating extends StatelessWidget {
-  const BookRating({super.key});
-
+  const BookRating({
+    super.key,
+    this.mainAxisAlignment = MainAxisAlignment.start,
+  });
+  final MainAxisAlignment mainAxisAlignment;
   @override
   Widget build(BuildContext context) {
     return Row(
+      mainAxisAlignment: mainAxisAlignment,
       children: [
-        const Icon(Icons.star, color: Colors.amber),
+        const Icon(Icons.star, color: Colors.amber, size: 20),
         const SizedBox(width: 6.3),
         const Text('4.8', style: Styles.txtStyle16),
         const SizedBox(width: 5),
-        Text(
-          '(245)',
-          style: Styles.txtStyle14.copyWith(color: Color(0xff707070)),
-        ),
+        Opacity(opacity: 0.5, child: Text('(245)', style: Styles.txtStyle14)),
       ],
     );
   }
