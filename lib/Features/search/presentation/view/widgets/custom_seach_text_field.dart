4022245c8470c50b4c6c3a4f1165a5f901faef2d
Nooraldeen Al_Awsaji <ngalawsaji@gmail.com>
2025-12-15 19:21:09 +0300

    create custom searh text field

---
+++ lib/Features/search/presentation/view/widgets/custom_seach_text_field.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class CustomSearchTextfield extends StatelessWidget {
+  const CustomSearchTextfield({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      decoration: InputDecoration(
+        enabledBorder: OutlineInputBorder(
+          borderSide: BorderSide(color: Colors.white),
+          borderRadius: BorderRadius.circular(12),
+        ),
+        hintText: 'Search',
+        suffixIcon: IconButton(onPressed: () {}, icon: Icon(Icons.search)),
+      ),
+    );
+  }
+}
--- lib/Features/search/presentation/view/widgets/search_view_body.dart
+++ lib/Features/search/presentation/view/widgets/search_view_body.dart
@@ -1,9 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:free_books_reading/Features/search/presentation/view/widgets/custom_seach_text_field.dart';
 
 class SearchViewBody extends StatelessWidget {
   const SearchViewBody({super.key});
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: SafeArea(child: Container()));
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 30),
+      child: Column(children: const [CustomSearchTextfield()]),
+    );
   }
 }
