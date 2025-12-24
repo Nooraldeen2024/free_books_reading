ae843d85fe555cc8ffcd5c35025e0b448511c17f
Nooraldeen Al_Awsaji <ngalawsaji@gmail.com>
2025-12-24 21:14:52 +0300

    use cashed network image

--- lib/Features/home/presentation/view/widgets/custom_book_item.dart
+++ lib/Features/home/presentation/view/widgets/custom_book_item.dart
@@ -1,21 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:cached_network_image/cached_network_image.dart';
 
 class CustomBookImage extends StatelessWidget {
   const CustomBookImage({super.key, required this.imageUrl});
   final String imageUrl;
   @override
   Widget build(BuildContext context) {
-    return AspectRatio(
-      aspectRatio: 2.6 / 4,
-      child: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 5),
-        child: Container(
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(16),
-            image: DecorationImage(
-              fit: BoxFit.fill,
-              image: NetworkImage(imageUrl.replaceFirst('http://', 'https://')),
-            ),
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 5),
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(16),
+
+        child: AspectRatio(
+          aspectRatio: 2.6 / 4,
+
+          child: CachedNetworkImage(
+            fit: BoxFit.fill,
+            imageUrl: imageUrl,
+            placeholder: (context, url) =>
+                Center(child: const CircularProgressIndicator()),
+            errorWidget: (context, url, error) => Icon(Icons.error),
           ),
         ),
       ),
